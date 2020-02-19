3672fb40247049ed165961dbef833e00e625a157
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-02-19 06:26:25 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 === 0
-    return "X"
+  if turn_count(board) % 2 = 0
+    "X"
   else
-    return "O"
+    "O"
   end
 end
